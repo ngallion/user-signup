15bd674f88943f4244dd2cfbff32df506f4b80fc
Nick Gallion <nicholas.gallion@gmail.com>
2018-03-31 23:54:32 -0500

    added post method for form, set up placeholders in template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+import re
 
 app = Flask(__name__)
 
@@ -8,5 +9,27 @@ app.config['DEBUG'] = True
 @app.route('/')
 def index():
     return render_template('signup_form.html')
+
+@app.route('/', methods=['POST'])
+def submit():
+    
+    username = request.form('username')
+    password = request.form('password')
+    verify = request.form('verify')
+    email = request.form('email')
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+    if username == '':
+        username_error = "Please enter a username"
+    # elif re.match("^[a-zA-Z0-9_.-]+$", username) is None:
+    #     username_error = "Invalid username"
+
+    return render_template('signup_form.html', username=username, username_error=username_error, 
+        password_error=password_error, verify_error=verify_error, email=email,  
+        email_error=email_error)
 
 app.run()
